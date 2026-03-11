71b53211d72d868b9909119264ae57e88252351b
onyedika-anagha <paschalanagha@gmail.com>
2026-03-11 20:44:49 +0100

    Add Open Graph and Twitter metadata for improved SEO and social sharing. Include site icons and manifest updates for better branding and user experience in the Loctech Student Portal.

--- loctech-student/app/layout.tsx
+++ loctech-student/app/layout.tsx
@@ -31,6 +31,37 @@ export const metadata: Metadata = {
   ],
   authors: [{ name: "Loctech", url: "https://loctech.com" }],
   creator: "Loctech",
+  openGraph: {
+    title: "Loctech Student | Attendance & Notifications",
+    description:
+      "Loctech Student Portal - Access your classes, take exams, and manage your attendance.",
+    url: "https://loctech-student.vercel.app",
+    siteName: "Loctech Student Portal",
+    images: [
+      {
+        url: "/logo/favicon-96x96.png",
+        width: 96,
+        height: 96,
+        alt: "Loctech Student Portal Logo",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary",
+    title: "Loctech Student Portal | Attendance & Notifications",
+    description:
+      "Loctech Student Portal - Access your classes, take exams, and manage your attendance.",
+    images: ["/logo/favicon-96x96.png"],
+    creator: "@onyedikaanagha_",
+  },
+  icons: {
+    icon: "/logo/favicon.ico",
+    shortcut: "/logo/favicon.ico",
+    apple: "/logo/apple-touch-icon.png",
+  },
   manifest: "/logo/site.webmanifest",
 };
 
